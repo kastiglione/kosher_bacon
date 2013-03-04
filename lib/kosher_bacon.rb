a4bce4e179a5bdf0a7f5be346e90349f6d0a0034
Dave Lee <davelee.com@gmail.com>
2013-03-04 16:48:41 -0700

    Add Motion::Project config block

--- lib/kosher_bacon.rb
+++ lib/kosher_bacon.rb
@@ -1,5 +1,13 @@
-require "kosher_bacon/version"
-
-module KosherBacon
-  # Your code goes here...
+if defined? Motion::Project::App
+  Motion::Project::App.setup do |config|
+    # Inject helpers that provide a (Mini)?Test::Unit adaptor
+    __dir__ = File.dirname(File.realpath(__FILE__))
+    helpers = Dir[File.join(__dir__, 'kosher_bacon', '*.rb')]
+    # Place kosher_bacon helpers immediately before the app's spec files.
+    # An app's spec files are immediately after RubyMotion's.
+    before_app_specs = config.spec_files.index { |file| !file.start_with?(config.motiondir) }
+    config.spec_files.insert(before_app_specs, *helpers)
+  end
+else
+  raise 'This file must be required within a RubyMotion project Rakefile.'
 end

2ff2f12c075e64bcb41d8d40246966460bc950c4
Dave Lee <davelee.com@gmail.com>
2013-03-04 17:21:09 -0700

    Restrict helpers to just kosher_bacon/adaptor.rb

--- lib/kosher_bacon.rb
+++ lib/kosher_bacon.rb
@@ -2,7 +2,7 @@ if defined? Motion::Project::App
   Motion::Project::App.setup do |config|
     # Inject helpers that provide a (Mini)?Test::Unit adaptor
     __dir__ = File.dirname(File.realpath(__FILE__))
-    helpers = Dir[File.join(__dir__, 'kosher_bacon', '*.rb')]
+    helpers = Dir[File.join(__dir__, 'kosher_bacon', 'adaptor.rb')]
     # Place kosher_bacon helpers immediately before the app's spec files.
     # An app's spec files are immediately after RubyMotion's.
     before_app_specs = config.spec_files.index { |file| !file.start_with?(config.motiondir) }
